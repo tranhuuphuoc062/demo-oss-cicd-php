cd2d1bd8a041243018f8dd79393dd3b76d23dc2d
huupuhoc <huuphuoc622004@gmail.com>
2025-12-16 23:53:28 +0700

    Khoi tao du an

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Báo cáo Bài tập 1: Demo CI/CD</h1>
+    <p>Trạng thái: Version 1 - Chưa có tự động hóa.</p>
+    <?php echo "Ngày giờ hiện tại: " . date("Y-m-d H:i:s"); ?>
+</body>
+</html>
