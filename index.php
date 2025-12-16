56e79fc8e89a0b1bc39bb998f4b8db95e31736bf
tranhuuphuoc062 <huuphuoc622004@gmail.com>
2025-12-17 01:18:28 +0700

    Update index.php
    Test changes

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <body>
     <h1>Báo cáo Bài tập 1: Demo CI/CD</h1>
+    <h2> Trần Hữu Phước </h2>
     <p>Trạng thái: Version 1 - Chưa có tự động hóa.</p>
     <?php echo "Ngày giờ hiện tại: " . date("Y-m-d H:i:s"); ?>
 </body>
