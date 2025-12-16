69b2342f09338a9d9e922bde734213a58dcbbd43
huupuhoc <huuphuoc622004@gmail.com>
2025-12-17 02:27:11 +0700

    Them ket noi Database

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,18 @@
 <?php
-$servername = "sql100.infinityfree.com"; // Lấy trong mục MySQL Details
-$username = "if0_40698005";
-$password = "eHcK8SXMxl3i";
-$dbname = "if0_40698005_test_db";
+// Thay 4 dòng dưới đây bằng thông tin thật của bạn từ InfinityFree
+$servername = "sql100.infinityfree.com"; // Thay bằng MySQL Host Name
+$username = "if0_40698005";              // Thay bằng MySQL User Name
+$password = "eHcK8SXMxl3i";          // Thay bằng Password (lấy ở FTP Details)
+$dbname = "if0_40698005_demo";           // Thay bằng MySQL Database Name
 
 // Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Kiểm tra
+// Kiểm tra kết nối
 if ($conn->connect_error) {
   die("Kết nối thất bại: " . $conn->connect_error);
 }
-echo "Kết nối Database thành công!";
-?>  
+echo "<h1>Kết nối Database thành công!</h1>";
+echo "<p>Host: " . $servername . "</p>";
+echo "<p>DB Name: " . $dbname . "</p>";
+?>
